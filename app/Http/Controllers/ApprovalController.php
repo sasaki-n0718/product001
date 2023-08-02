54b671385270b005958515c9ec5b4b7d40f40a05
sasaki-n0718 <dddobrat@gmail.com>
2023-08-02 19:22:18 +0900

    list_page

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class ApprovalController extends Controller
 {
-    public function home()
+    public function home(Post $post)
     {
-        return view('approval.home');
+        return view('approval.home')->with(['posts'=>$post->getPaginate()]);
     }
     public function post()
     {
--- resources/views/approval/home.blade.php
+++ resources/views/approval/home.blade.php
@@ -3,7 +3,21 @@
     
 <body>
     <h1>home page</h1>
-    
+    <div class='list'>
+        @foreach ($posts as $post)
+            <div class='post'>
+                <h2 class='title'>{{ $post->title }}</h2>
+                <p class='user'>{{ $post->user->name }}</p>
+                //ユーザー名を表示させたい。
+            </div>
+        @endforeach
+        <div class="pagination">
+            {{$posts->links()}}
+        </div>
+        <div class="make_approval">
+            <a href="/post">新規決裁の作成</a>
+        </div>
+    </div>
 </body>
 
 
