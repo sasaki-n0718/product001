419c83e17b4dcaac2fb5b7c4adc77de50cb7bf63
sasaki-n0718 <dddobrat@gmail.com>
2023-12-01 10:26:42 +0900

    検索機能

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Post extends Model
 {
     use HasFactory;
     
-    public function getPaginate(int $limit_count=10)
+    public function getPaginate(int $limit_count=5)
     {
         return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+    }
+    public function searchIndex($request,int $limit_count=5)
+    {
+        $keyword=$request->input('keyword');
+        $query=Post::query();
+        if(!empty($keyword)){
+            $query->where('title','LIKE',"%{$keyword}%")->get();
+        }
+        return $posts=$query->orderBy('updated_at','DESC')->paginate($limit_count);
     }
     protected $fillable =[
         'title',
